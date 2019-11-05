66d9d47a320e10044e7096875dbf5efb798e39a2
igorthedci <foumart@ukr.net>
2019-11-05 13:13:44 +0200

    Hillel 10 Homework. Web driver, part 1

---
+++ src/main/java/web_driver_01/Main.java
@@ -0,0 +1,18 @@
+package web_driver_01;
+
+/**
+ * (presentation, page 164)
+ *
+ * Открыть главную страницу automationpractice.com
+ * Открыть форму регистрации нового клиента (Sign in )
+ * В секции Create an account ввести корректный емейл
+ * Нажать Create an account
+ * На форме регистрации заполнить ВСЕ ОБЯЗАТЕЛЬНЫЕ поля , КРОМЕ
+ * выпадающих списков
+ *
+ * Нажать Register
+ * Проверить что сообщение валидации содержит текст ,
+ * например “There are 3 errors”
+ */
+public class Main {
+}
